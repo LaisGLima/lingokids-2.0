d98979133032cc6a18ab4ee4aafdf9f05393d806
Lais Lima <115181929+LaisGLima@users.noreply.github.com>
2024-06-04 12:58:30 -0300

    Update AlimentosScreen.js

--- components/abas/AlimentosScreen.js
+++ components/abas/AlimentosScreen.js
@@ -172,7 +172,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   alimentoName: {
-    fontSize: 16,
+    fontSize: 20,
     fontWeight: 'bold',
     color: 'white',
     textShadowColor: 'black', 
@@ -198,7 +198,7 @@ const styles = StyleSheet.create({
     marginHorizontal: 10,
   },
   paginationText: {
-    fontSize: 16,
+    fontSize: 20,
     fontWeight: 'bold',
     color: 'white',
     marginHorizontal: 10,
